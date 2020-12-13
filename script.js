8fddf257a80c858ec43f9a6b2fb88af59f9c7398
Tina Nguyen <nguyentinaca@yahoo.com>
2020-12-13 06:40:44 -0800

    displays in comsole, remove commas in number chosen only

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // array in parameters
 var symbolcharInfo = ['~','!','@','#','%','^','&','*','(',')','_','+'];
-var numbercharInfo = [0,1,2,3,4,5,6,7,8,9];
+var numbercharInfo = ["0123456789"];
 var lowercaseInfo = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var uppercaseInfo = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var Length = "";
@@ -57,17 +57,18 @@ function writePassword() {
       passwordOption = passwordOption + uppercaseInfo
     }
     //show parameters combination in console
-    console.log(passwordOption)
+    console.log(passwordOption);
     
      // randomize parameter with length entered
      var randomPassword = "";
      
-     for (let i = 0; i < Length; ++i) {
+     for (let i = 0; i < includeLength; ++i) {
           randomPassword = randomPassword + passwordOption[Math.floor(Math.random() * passwordOption.length)];
+          //display random password combination in console per length ented
           console.log(randomPassword);
      }
      return randomPassword;
-   }
+   };
    
    //display password in box
     function generatePassword() {
