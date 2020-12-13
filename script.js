7e255a92e2b9bfc2d7d76721331746748c5b4bf5
Tina Nguyen <nguyentinaca@yahoo.com>
2020-12-13 00:42:13 -0800

    questions for pw pop up when click

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 // Assignment code here
-var lowerCase = 'abcdefghijklmnopqrstuvwyz';
-var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var numberChar = '0123456789';
-var symbolChar = '~!@#$%^&*()_+';
+var lowercaseInfo = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var uppercaseInfo = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numbercharInfo = [0,1,2,3,4,5,6,7,8,9];
+var symbolcharInfo = ['~','!','@','#','%','^','&','*','(',')','_','+'];
+var includeLength = "";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var length = window.prompt("How many characters would you like your password to contain?");
-    var includesymboleChar = confirm("Click OK to confirm including special.");
-    var includenumberChar = confirm("Click OK to confirm including numeric characters.");
-    var includelowerCase = confirm("Click OK to confirm including lowercase characters.");
-    var includeupperCase = confirm("Click OK to confirm including uppercase characters.");
-    return;
+    var includeLength = window.prompt("How many characters would you like your password to contain?");
+    while(includeLength <=8 || includeLength >= 128){
+      window.alert("Password length must be between 8 to 128 characters.");
+      var includeLength = window.prompt("How many characters would you like your password to contain?");
+    }
 
-   var password = {
-      length: length,
-      symbolChar: includesymboleChar,
-      numberChar: includenumberChar,
-      lowerCase: includelowerCase,
-      upperCase = includeupperCase
-   }
+    var includesymboleChar = window.confirm("Click OK to confirm including special.");
+    var includenumberChar = window.confirm("Click OK to confirm including numeric characters.");
+    var includelowerCase = window.confirm("Click OK to confirm including lowercase characters.");
+    var includeupperCase = window.confirm("Click OK to confirm including uppercase characters.");
+    while(includesymboleChar ===false && includenumberChar === false&& includelowerCase === false && includeupperCase === false){
+      alert("You must choose at least one parameter");
+    var includesymboleChar = window.confirm("Click OK to confirm including special.");
+    var includenumberChar = window.confirm("Click OK to confirm including numeric characters.");
+    var includelowerCase = window.confirm("Click OK to confirm including lowercase characters.");
+    var includeupperCase = window.confirm("Click OK to confirm including uppercase characters.");
+    }
+   
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+
   };
-  
-
-  
-  //var passwordText = document.querySelector("#password");
-
-  //passwordText.value = password;
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
 
