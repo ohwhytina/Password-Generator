1f047f116eeefb689af57e8a8793990d48202564
Tina Nguyen <nguyentinaca@yahoo.com>
2020-12-13 06:34:03 -0800

    add var to randomized chosen parameter

--- script.js
+++ script.js
@@ -1,35 +1,79 @@
-// Assignment code here
+// array in parameters
+var symbolcharInfo = ['~','!','@','#','%','^','&','*','(',')','_','+'];
+var numbercharInfo = [0,1,2,3,4,5,6,7,8,9];
 var lowercaseInfo = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var uppercaseInfo = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numbercharInfo = [0,1,2,3,4,5,6,7,8,9];
-var symbolcharInfo = ['~','!','@','#','%','^','&','*','(',')','_','+'];
-var includeLength = "";
+var Length = "";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+    //pop up to enter length
     var includeLength = window.prompt("How many characters would you like your password to contain?");
-    while(includeLength <=8 || includeLength >= 128){
+    //must be between 8-128 characters, loop if enter outside of 8-128
+    while(includeLength <=8 && includeLength >= 128) {
       window.alert("Password length must be between 8 to 128 characters.");
+      //try again
       var includeLength = window.prompt("How many characters would you like your password to contain?");
     }
-
-    var includesymboleChar = window.confirm("Click OK to confirm including special.");
+    //add symbol characters
+    var includesymboleChar = window.confirm("Click OK to confirm including special characters.");
+    //add numberic characters
     var includenumberChar = window.confirm("Click OK to confirm including numeric characters.");
+    //add lowercase characters
     var includelowerCase = window.confirm("Click OK to confirm including lowercase characters.");
+    //add uppercase characters
     var includeupperCase = window.confirm("Click OK to confirm including uppercase characters.");
-    while(includesymboleChar ===false && includenumberChar === false&& includelowerCase === false && includeupperCase === false){
-      alert("You must choose at least one parameter");
+    //if not chosen, don't add parater
+    while(
+      includesymboleChar ===false 
+      && includenumberChar === false
+      && includelowerCase === false 
+      && includeupperCase === false)
+      {
+      //must include 1 parameter
+      window.alert("You must choose at least one parameter");
+      //try again, chosen at least 1
     var includesymboleChar = window.confirm("Click OK to confirm including special.");
     var includenumberChar = window.confirm("Click OK to confirm including numeric characters.");
     var includelowerCase = window.confirm("Click OK to confirm including lowercase characters.");
     var includeupperCase = window.confirm("Click OK to confirm including uppercase characters.");
     }
+    
+   //combine chosen parmeter(s)
+    var passwordOption = []
+    if (includesymboleChar) {
+      passwordOption = passwordOption + symbolcharInfo
+    }
+    if (includenumberChar) {
+      passwordOption = passwordOption + numbercharInfo
+    }
+    if (includelowerCase) {
+      passwordOption = passwordOption + lowercaseInfo
+    }
+    if (includeupperCase) {
+      passwordOption = passwordOption + uppercaseInfo
+    }
+    //show parameters combination in console
+    console.log(passwordOption)
+    
+     // randomize parameter with length entered
+     var randomPassword = "";
+     
+     for (let i = 0; i < Length; ++i) {
+          randomPassword = randomPassword + passwordOption[Math.floor(Math.random() * passwordOption.length)];
+          console.log(randomPassword);
+     }
+     return randomPassword;
+   }
    
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+   //display password in box
+    function generatePassword() {
+    var password = writePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 
   };
 
